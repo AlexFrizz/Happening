e8db3ca693cf002025bc2801097d6a2c4497951e
Alex Frizziero <alex.frizziero@edu.unife.it>
2023-11-16 17:28:04 +0100

    auth - user

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([[
+            'name'          => 'Alex',
+            'surname'       => 'Frizziero',
+            'email'         => 'alex@happening.com',
+            'password'      =>  bcrypt('password'),
+            'created_at'    =>  date('Y-m-d h:i:s'),
+            'updated_at'    =>  date('Y-m-d h:i:s')
+        ], [
+            'name'          => 'Giacomo',
+            'surname'       => 'Piva',
+            'email'         => 'giacomo@happening.com',
+            'password'      =>  bcrypt('password'),
+            'created_at'    =>  date('Y-m-d h:i:s'),
+            'updated_at'    =>  date('Y-m-d h:i:s')
+        ], [
+            'name'          => 'Greta',
+            'surname'       => 'Finotti',
+            'email'         => 'greta@happening.com',
+            'password'      =>  bcrypt('password'),
+            'created_at'    =>  date('Y-m-d h:i:s'),
+            'updated_at'    =>  date('Y-m-d h:i:s')
+        ]]);
+    }
+}
